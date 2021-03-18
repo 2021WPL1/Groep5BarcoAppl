dc55fa39bad98e94f4b1f675363265fa4909cc30
robbe.delsoir <robbe.delsoir@student.vives.be>
2021-03-18 12:18:33 +0100

    Label

--- BarcoApplicatie/MainWindow.xaml.cs
+++ BarcoApplicatie/MainWindow.xaml.cs
@@ -587,7 +587,7 @@ namespace BarcoApplicatie
 
         private void txtEutPartnumber4_TextChanged(object sender, TextChangedEventArgs e)
         {
-            errorHandling.ControlInput("[^0-9]", txtEutPartnumber4, numbersErrorLabel, "Please enter numbers only.");
+            errorHandling.ControlInput("[^0-9]", txtEutPartnumber4, numbersErrorLabel,  "Please enter numbers only.");
         }
 
         private void txtEutPartnumber5_TextChanged(object sender, TextChangedEventArgs e)
