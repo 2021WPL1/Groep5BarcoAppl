c93a2c817e71c82cc47dfacf16ba279343c357ec
nikki <nikkinoppe@student.vives.be>
2021-05-25 13:27:23 +0200

    cleanup & added comment

--- BarcoApplicatie/Email.cs
+++ BarcoApplicatie/Email.cs
@@ -12,16 +12,12 @@ using System.Windows.Threading;
 
 namespace BarcoApplicatie
 {
-    //Nikki
+
     class Email
     {
         private BarcoApplicationDataService _dataService;
 
-        //initialise for SendMail
-        private static string mailFrom = "Groep3testprog@gmail.com";
-        private static string mailFromPassword = "Testtest123";
-
-
+        //Nikki
         public void ActivateEmail()
         {
             _dataService = BarcoApplicationDataService.Instance();
@@ -31,57 +27,53 @@ namespace BarcoApplicatie
             timer.Tick += timer_Tick;
             timer.Start();
         }
-
-        //tijdelijk
+        //Nikki
         void timer_Tick(object sender, EventArgs e)
         {
             //Date kan aangepast worden naar keuze
             DateTime emailSendTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 11, 28, 0); //18pm,0min,0sec
             DateTime dateNow = DateTime.Now;
 
-            //List<RqRequest> listAllRequests = _dataService.getAllRequests();
-            //if (listAllRequests != null)
-            //{
-
+            //geen mail op zaterdag & zondag
             if (dateNow.DayOfWeek != DayOfWeek.Saturday || dateNow.DayOfWeek != DayOfWeek.Sunday)
             {
                 if (emailSendTime.Hour == dateNow.Hour && emailSendTime.Minute == dateNow.Minute)
                 {
                     SendMail();
                 }
-            }
-            
-            //}
+            }    
+
         }
 
+        //initialise for SendMail
+        private static string mailFrom = "Groep3testprog@gmail.com";
+        private static string mailFromPassword = "Testtest123";
+
+        //Nikki
         public void SendMail()
         {
-            List<RqRequest> listAllRequests = _dataService.getAllRequests();
-
             //setup smtp & mail 
-            using (SmtpClient client = new SmtpClient("smtp.gmail.com", 587))
+            using SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+            try
             {
-                try
-                {
-                    client.EnableSsl = true;
-                    client.DeliveryMethod = SmtpDeliveryMethod.Network;
-                    client.UseDefaultCredentials = false;
-                    client.Credentials = new NetworkCredential(mailFrom, mailFromPassword);
+                client.EnableSsl = true;
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.UseDefaultCredentials = false;
+                client.Credentials = new NetworkCredential(mailFrom, mailFromPassword);
 
-                    MailMessage mail = new MailMessage();
-                    mail.From = new MailAddress(mailFrom);
-                    mail.To.Add("nikki.noppe@student.vives.be");
+                MailMessage mail = new MailMessage();
+                mail.From = new MailAddress(mailFrom);
+                mail.To.Add("nikki.noppe@student.vives.be");
 
-                    //needs to be updated with info!
-                    mail.Subject = "testtesttes";
-                    mail.Body = "Dit is een automatische mail" + listAllRequests;
+                //needs to be updated with info!
+                mail.Subject = "testtesttes";
+                mail.Body = "Dit is een automatische mail";
 
-                    client.Send(mail);
-                }
-                catch (Exception)
-                {
-                    throw;
-                }
+                client.Send(mail);
+            }
+            catch (Exception)
+            {
+                throw;
             }
         }
     }
