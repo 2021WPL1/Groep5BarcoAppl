9d36f7d1def7f4b4e7913f2042d23c49681eace3
koent <koent@LAPTOP>
2021-06-02 17:27:16 +0200

    added euts to acceptJRWindow

--- BarcoApplicatie/AcceptJobrequest.xaml.cs
+++ BarcoApplicatie/AcceptJobrequest.xaml.cs
@@ -1,15 +1,6 @@
-﻿using BarcoApplication.Data;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using BarcoApplicatie.viewModels;
 
 namespace BarcoApplicatie
