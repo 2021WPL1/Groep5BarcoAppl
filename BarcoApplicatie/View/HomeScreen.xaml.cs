fb2674a08e53f4d858a98da78713d204e7dc1627
robbe.delsoir <robbe.delsoir@student.vives.be>
2021-06-03 09:12:49 +0200

    fixed errormessage

--- BarcoApplicatie/View/HomeScreen.xaml.cs
+++ BarcoApplicatie/View/HomeScreen.xaml.cs
@@ -27,12 +27,16 @@ namespace BarcoApplicatie
             //Robbe
             //De registry ophalen en de knoppen tonen adhv wie er op welke dingen mag
             RegistryKey request = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\\BarRequest");
-            string userName = Convert.ToString(request.GetValue("Name"));
+            string userName = "";
+
+            if (request != null)
+            {
+                userName = Convert.ToString(request.GetValue("Name"));
+            }
             if (userName == "Robbe Delsoir")
             {
                 btnSendJob1_Copy.IsEnabled = true;
             }
-
             if (userName == "Nikki Noppe")
             {
                 btnSendJob1_Copy1.IsEnabled = true;
